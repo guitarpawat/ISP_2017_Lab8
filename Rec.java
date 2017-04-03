42bf6a2e8fc1a986e8f6bc99c42c4fc54ea74eea
Nitith Chayakul <c.nitith@gmail.com>
2017-04-03 13:37:54 +0700

    implements removeChar method

--- Rec.java
+++ Rec.java
@@ -6,9 +6,9 @@ public class Rec {
 		numDigits(1356) = 4.
 		Precondition: n >= 0. */
     public static int numDigits(int n) {
-	
+
 	// fill me in
-	
+
     }
 
     /** = sum of the digits in the decimal representation of n.
@@ -16,25 +16,25 @@ public class Rec {
       sumDigits(345) = 12.
       Precondition: n >= 0. */
     public static int sumDigits(int n) {
-	
+
 	// fill me in
-	
+
     }
-	
+
     /** = a copy of s with to_remove_char removed.
 		Example: removeChar("abeabe", 'e') = "abab". */
 	public static String removeChar(String s, char to_remove_char) {
-	
-	// fill me in
-	
+        if( s.isEmpty() ) return "";
+        if( s.charAt(0) == to_remove_char ) return remove(s.substring(1), to_remove_char);
+        return s.charAt(0) + removeChar(s.substring(1) , to_remove_char);
 	}
-	
+
     /** = a copy of s with characters in reverse order.
 		Example: reverse("abcdefg") = "gfedcba". */
 	public static String reverse(String s) {
-	
+
 	// fill me in
-	
+
 	}
 
     public static void main(String[] args) {
@@ -50,7 +50,7 @@ public class Rec {
 	System.out.println("-----------------");
 	System.out.println(removeChar("eawabbcceccddeeaaeeeee", 'e'));
 	System.out.println(removeChar("acacacacac", 'a'));
-	System.out.println("-----------------");		
+	System.out.println("-----------------");
 	System.out.println(reverse("aaabbbaccabda"));
         System.out.println(reverse("abbcccdeaaa"));
 	System.out.println(reverse("AMANAPLANACANALPANAMA"));
